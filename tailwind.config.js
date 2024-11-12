0cbd1e47879c6d84a7ead180c4404b5c9d14168c
tomatofrog1 <alcosharley21@gmail.com>
2024-11-13 07:24:17 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fcbf49",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+      backgroundImage: {
+        bodyBanner:
+          "linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.0)),  url('../img/Banner-img.png')",
+        banner2: "url('../img/bg-trees.png')",
+      },
+    },
+  },
+  plugins: [],
+};
